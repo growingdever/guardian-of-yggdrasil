91c9aba536543233fd62f02a347ae3352ff42510
loki <growingdever@gmail.com>
2015-01-28 21:00:21 +0900

    시속 속력으로 unit 변환 함수를 static으로 가지는 계산기 클래스 만듬.

--- Assets/Scripts/FlightController.cs
+++ Assets/Scripts/FlightController.cs
@@ -7,7 +7,7 @@ public class FlightController : MonoBehaviour
 	public float RotationPowerYaw = 0.3f;
 	public float RotationPowerPitch = 0.3f;
 
-	public float UnitPerMeter = 10;
+	public float UnitPerMeter = 1.0f;
     public float KilometerPerHour = 180.0f;
 	public float RollDampingFactor = 1.0f;
 
@@ -32,11 +32,7 @@ public class FlightController : MonoBehaviour
 		}
 		this.transform.Rotate(0, 0, rollDamping * Time.deltaTime * RollDampingFactor);
 
-
-		float meterPerSecond = KilometerPerHour * MeterPerKilometer / SecondsPerHour;
-		float unitPerSecond = meterPerSecond / UnitPerMeter;
-		float fps = 1.0f / Time.deltaTime;
-		float speedFactor = unitPerSecond / fps;
+		float speedFactor = UnitCalculator.ToUnitFactorFromVelocity(KilometerPerHour, UnitPerMeter, MeterPerKilometer, SecondsPerHour);
 		this.transform.position += this.transform.forward * speedFactor;
 	}
 }
---
+++ Assets/Scripts/UnitCalculator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitCalculator {
+
+
+
+	public static float ToUnitFactorFromVelocity(float kmPerHour, float unitPerMeter = 1.0f, int meterPerKM = 1000, int secondsPerHour = 3600) {
+		float meterPerSecond = kmPerHour * meterPerKM / secondsPerHour;
+		float unitPerSecond = meterPerSecond / unitPerMeter;
+		float fps = 1.0f / Time.deltaTime;
+		float speedFactor = unitPerSecond / fps;
+		return speedFactor;
+	}
+	
+}
