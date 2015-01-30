dc888a44e5f8c915aa7ec63b98172884b80a6861
loki <growingdever@gmail.com>
2015-01-30 16:17:04 +0900

    속도 표시 UI 추가.

---
+++ Assets/Scripts/SpeedLabelUpdate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedLabelUpdate : MonoBehaviour {
+
+	public FlightController Flight;
+
+	UILabel Label;
+
+	void Start () {
+		Label = this.GetComponent<UILabel>();
+	}
+
+	void Update () {
+		Label.text = Flight.KilometerPerHour + " km/h";
+	}
+}
