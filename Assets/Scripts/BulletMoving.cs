cf99cb88101f92e3510d45affbda377fabc1d8ff
loki <growingdever@gmail.com>
2015-01-30 17:25:40 +0900

    bullet과 missile을 다르게 해서 쏜다.

---
+++ Assets/Scripts/BulletMoving.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletMoving : MonoBehaviour {
+
+	public float Speed;
+	
+	void Update () {
+		// this is very special case...
+		this.transform.position += this.transform.up * Speed;
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		print (collision);
+	}
+
+}
--- Assets/Scripts/ShootBullet.cs
+++ Assets/Scripts/ShootBullet.cs
@@ -4,12 +4,25 @@ using System.Collections;
 public class ShootBullet : MonoBehaviour {
 
 	public GameObject BulletContainer;
-	public GameObject PrefabBullet;
 	public Transform SpawnPoint;
+	public GameObject PrefabBullet;
+	public GameObject PrefabMissile;
 
+	// below two speed is not unified....
+	public float BulletSpeed = 3.0f;
 	public float MissileSpeed = 500.0f;
 	public float ForwardOffsetFactor = 1000.0f;
 	public float ColliderRadius = 5.0f;
+
+	public enum WeaponType {
+		Bullet,
+		Missile,
+	}
+
+	public WeaponType CurrentWeapon {
+		get;
+		private set;
+	}
 
 	Camera _playerCamera;
 
@@ -24,11 +37,34 @@ public class ShootBullet : MonoBehaviour {
 
 	void Update () {
 		if (Input.GetButtonDown("Fire1")) {
-			SpawnBullet(SpawnPoint);
+			if( CurrentWeapon == WeaponType.Bullet ) {
+				SpawnBullet(SpawnPoint);
+			} else if( CurrentWeapon == WeaponType.Missile ) {
+				SpawnMissile(SpawnPoint);
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.Keypad1)) {
+			CurrentWeapon = WeaponType.Bullet;
+		}
+		if (Input.GetKeyDown(KeyCode.Keypad2)) {
+			CurrentWeapon = WeaponType.Missile;
 		}
 	}
 
 	void SpawnBullet(Transform spawnPoint) {
+		GameObject clone = Instantiate (PrefabBullet, spawnPoint.position, spawnPoint.rotation) as GameObject;
+		clone.transform.parent = BulletContainer.transform;
+		clone.transform.Rotate(90, 0, 0);
+
+		BulletMoving comp = clone.GetComponent<BulletMoving>();
+		comp.Speed = BulletSpeed;
+
+		StartCoroutine (DestroyBullet (clone));
+	}
+
+
+	void SpawnMissile(Transform spawnPoint) {
 		GameObject clone = Instantiate (PrefabBullet, spawnPoint.position, spawnPoint.rotation) as GameObject;
 		clone.transform.parent = BulletContainer.transform;
 
@@ -53,13 +89,11 @@ public class ShootBullet : MonoBehaviour {
 	}
 
 	void OnMissileCollisionEnter(object sender, CollisionInfo e) {
-		print (sender + "\n" + e.Hit);
 		Enemy enemy = e.Hit.collider.GetComponent<Enemy>();
 		if( enemy != null ) {
 			// this collider is enemy!
 			GameObject goEnemy = enemy.gameObject;
 			enemy.HP -= 10;
-			print (enemy);
 		}
 	}
 
