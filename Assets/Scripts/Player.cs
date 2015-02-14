3dcdf7c3ba8865ba90bfe9e43f4731a0cd71e872
loki <growingdever@gmail.com>
2015-02-14 20:38:41 +0900

    플레이어, 이그드라실 hp bar 추가.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public UISlider SliderHP;
+	
+	
+	public int MaxHP {
+		get;
+		set;
+	}
+	
+	int _hp;
+	public int HP {
+		get {
+			return _hp;
+		}
+		set {
+			_hp = value;
+			if( _hp <= 0 ) {
+				SliderHP.value = 1.0f * HP / MaxHP;
+			}
+		}
+	}
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
